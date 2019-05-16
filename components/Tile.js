f5c69a9b9b7fab0656858c32624092367ca8f325
Koby Kilimnik <koby.kilimnik@imperva.com>
2019-05-16 18:01:51 +0300

    redux magic works

--- components/Tile.js
+++ components/Tile.js
@@ -4,22 +4,16 @@ const get_mode = require('../utils/helper.js').get_mode;
 import { fireZeeMissile } from "../lib/redux/reducers/boards";
 
 class Tile extends React.Component {
-			state = {
-                value: this.props.value,
-                mode: this.props.mode
-			}
+
             handleClick = (event) =>  {
                 this.props.dispatch(
 			        fireZeeMissile(this.props.row, this.props.col)
                 )
-              let new_value = this.props.boards.board[this.props.row][this.props.col]
-              let new_mode = get_mode(new_value);
-              this.setState({mode: new_mode, value: new_value});
             }
 			render() {
 				return (
-				<div style={styles.tile.get(this.state.mode)} onClick={this.handleClick}>
-                     {this.state.value}
+				<div style={styles.tile.get(this.props.mode)} onClick={this.handleClick}>
+                     {this.props.value}
 				</div>);
 			}
 }
